a82438e1d843cae371c17e72ec5367092fba0205
Suresh <100891096+mesureshdub@users.noreply.github.com>
2022-05-26 18:26:26 -0600

    Update stack.c
    Stack is a linear data structure that follows a particular order in which the operations are performed. The order may be LIFO(Last In First Out) or FILO(First In Last Out).
    
    Mainly the following four basic operations are performed in the stack:
    
    Push: Adds an item to the stack. If the stack is full, then it is said to be an Overflow condition.
    Pop: Removes an item from the stack. The items are popped in the reversed order in which they are pushed. If the stack is empty, then it is said to be an Underflow condition.

--- stack.c
+++ stack.c
@@ -1,17 +1,19 @@
+//***************************************** Implementing Stacks Operations  ************************************************
+
 #include<stdio.h>
 #include<stdlib.h>
  
-#define MAX 5 //Maximum number of elements that can be stored
- 
+#define MAX 5                           //Maximum number of elements that can be stored
+                                        // Here we assign the value of top in stack is -1.
 int top=-1,stack[MAX];
-void push();
-void pop();
-void display();
+void push();                            //Push is used to add elements in stack.
+void pop();                             //Pop is used to del. elements in stack.
+void display();                         //Display is used to view elements in stack.
  
 int main()
 {
 int ch;
-while(1) //infinite loop, will end when choice will be 4
+while()                                //infinite loop, will end when choice will be 4
 {
 printf("\n*********** Stack Menu ***********");
 printf("\n\n1.Push\n2.Pop\n3.Display\n4.Exit");
@@ -30,10 +32,11 @@ default: printf("\nWrong Choice!!");
 }
 }
 }
- 
+                                        //Here we Call Push Function to add Elements in Stacks.
+
 void push()
 {
-int val;
+int value;
 if(top==MAX-1)
 {
 printf("\n********Stack is full!********");
@@ -41,14 +44,17 @@ printf("\n********Stack is full!********");
 else
 {
 printf("\nHey! Enter the element to push:");
-scanf("%d",&val);
+scanf("%d",&value);
 top=top+1;
-stack[top]=val;
+stack[top]=value;
 }
 }
- 
+                                        //Here we Call Pop Function to del. Elements in Stacks.
+
+
 void pop()
 {
+                                        //Here we use condition to check if top is equal to -1.
 if(top==-1)
 {
 printf("\nStack is empty!!");
@@ -59,18 +65,21 @@ printf("\nDeleted element is %d",stack[top]);
 top=top-1;
 }
 }
- 
+                                        //Here we Call Display Function to view Elements in Stacks.
+
 void display()
 {
-int i;
+int index;
+                                        //Here we use condition to check if Stack is Empty or Not.
 if(top==-1)
 {
-printf("\nBuddy Stack is empty!!");
+printf("\nOh! Stack is empty!!");
 }
-else
+else            
 {
 printf("\nElements in Stack is...\n");
-for(i=top;i>=0;--i)
-printf("%d\n",stack[i]);
+
+for(index=top;index>=0;--index)
+printf("%d\n",stack[index]);
 }
 }
