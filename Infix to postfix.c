d9194549e6ced177f86eb7ff735ed9785f3b6b6b
Suresh <100891096+mesureshdub@users.noreply.github.com>
2022-06-09 23:30:52 -0600

    Update Infix to postfix.c

--- Infix to postfix.c
+++ Infix to postfix.c
@@ -1,167 +1,184 @@
-/*  C Program to convert infix to postfix  */
-
+//Infix to Postfic Conversion
 #include<stdio.h>
+#include<stdlib.h>      
+#include<ctype.h>     /* for isdigit(char ) */
 #include<string.h>
-#include<math.h>
-#include<stdlib.h>
 
-#define BLANK ' '
-#define TAB '\t'
-#define MAX 50
+#define SIZE 100
 
-void push(long int symbol);
-long int pop();
-void infix_to_postfix();
-long int eval_post();
-int priority(char symbol);
-int isEmpty();
-int white_space(char );
+char stack[SIZE];
+int top = -1;
 
-char infix[MAX], postfix[MAX];
-long int stack[MAX];
-int top;
+/* define push operation */
 
+void push(char item)
+{
+	if(top >= SIZE-1)
+	{
+		printf("\nStack Overflow.");
+	}
+	else
+	{
+		top = top+1;
+		stack[top] = item;
+	}
+}
+
+/* define pop operation */
+char pop()
+{
+	char item ;
+
+	if(top <0)
+	{
+		printf("stack under flow: invalid infix expression");
+		getchar();
+		
+		exit(1);
+	}
+	else
+	{
+		item = stack[top];
+		top = top-1;r
+		return(item);
+	}
+}
+
+/* define function that is used to determine whether any symbol is operator or not
+(that is symbol is operand)
+* this fucntion returns 1 if symbol is opreator else return 0 */
+
+int is_operator(char symbol)
+{
+	if(symbol == '^' || symbol == '*' || symbol == '/' || symbol == '+' || symbol =='-')
+	{
+		return 1;
+	}
+	else
+	{
+	return 0;
+	}
+}
+
+/* define fucntion that is used to assign precendence to operator.
+* Here ^ denotes exponent operator.
+* In this fucntion we assume that higher integer value
+* means higher precendence */
+
+int precedence(char symbol)
+{
+	if(symbol == '^')/* exponent operator, highest precedence*/
+	{
+		return(3);
+	}
+	else if(symbol == '*' || symbol == '/')
+	{
+		return(2);
+	}
+	else if(symbol == '+' || symbol == '-')          /* lowest precedence */
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
+
+void InfixToPostfix(char infix_exp[], char postfix_exp[])
+{ 
+	int i, j;
+	char item;
+	char x;
+
+	push('(');                               /* push '(' onto stack */
+	strcat(infix_exp,")");                  /* add ')' to infix expression */
+
+	i=0;
+	j=0;
+	item=infix_exp[i];         /* initialize before loop*/
+
+	while(item != '\0')        /* run loop till end of infix expression */
+	{
+		if(item == '(')
+		{
+			push(item);
+		}
+		else if( isdigit(item) || isalpha(item))
+		{
+			postfix_exp[j] = item;              /* add operand symbol to postfix expr */
+			j++;
+		}
+		else if(is_operator(item) == 1)        /* means symbol is operator */
+		{
+			x=pop();
+			while(is_operator(x) == 1 && precedence(x)>= precedence(item))
+			{
+				postfix_exp[j] = x;                  /* so pop all higher precendence operator and */
+				j++;
+				x = pop();                       /* add them to postfix expresion */
+			}
+			push(x);
+			/* because just above while loop will terminate we have
+			oppped one extra item
+			for which condition fails and loop terminates, so that one*/
+
+			push(item);                 /* push current oprerator symbol onto stack */
+		}
+		else if(item == ')')         /* if current symbol is ')' then */
+		{
+			x = pop();                   /* pop and keep popping until */
+			while(x != '(')                /* '(' encounterd */
+			{
+				postfix_exp[j] = x;
+				j++;
+				x = pop();
+			}
+		}
+		else
+		{ /* if current symbol is neither operand not '(' nor ')' and nor
+			operator */
+			printf("\nInvalid infix Expression.\n");        /* the it is illegeal  symbol */
+			getchar();
+			exit(1);
+		}
+		i++;
+
+
+		item = infix_exp[i]; /* go to next symbol of infix expression */
+	} /* while loop ends here */
+	if(top>0)
+	{
+		printf("\nInvalid infix Expression.\n");        /* the it is illegeal  symbol */
+		getchar();
+		exit(1);
+	}
+	if(top>0)
+	{
+		printf("\nInvalid infix Expression.\n");        /* the it is illegeal  symbol */
+		getchar();
+		exit(1);
+	}
+
+
+	postfix_exp[j] = '\0'; /* add sentinel else puts() fucntion */
+	/* will print entire postfix[] array upto SIZE */
+
+}
+
+/* main function begins */
 int main()
 {
-        long int value;
-        top=-1;
-        printf("Enter infix : ");
-        gets(infix);
-        infix_to_postfix();
-        printf("Postfix : %s\n",postfix);
-        value=eval_post();
-        printf("Value of expression : %ld\n",value);
+	char infix[SIZE], postfix[SIZE];         /* declare infix string and postfix string */
 
-        return 0;
+	
+	printf("ASSUMPTION: The infix expression contains single letter variables and single digit constants only.\n");
+	printf("\nEnter Infix expression : ");
+	gets(infix);
 
-}/*End of main()*/
+	InfixToPostfix(infix,postfix);                   /* call to convert */
+	printf("Postfix Expression: ");
+	puts(postfix);                     /* print postfix expression */
 
-void infix_to_postfix()
-{
-        unsigned int i,p=0;
-        char next;
-        char symbol;
-        for(i=0;i<strlen(infix);i++)
-        {
-                symbol=infix[i];
-                if(!white_space(symbol))
-                {
-                        switch(symbol)
-                        {
-                        case '(':
-                                push(symbol);
-                                break;
-                        case ')':
-                                while((next=pop())!='(')
-                                        postfix[p++] = next;
-                                break;
-                        case '+':
-                        case '-':
-                        case '*':
-                        case '/':
-                        case '%':
-                        case '^':
-                                while( !isEmpty( ) &&  priority(stack[top])>= priority(symbol) )
-                                        postfix[p++]=pop();
-                                push(symbol);
-                                break;
-                        default: /*if an operand comes*/
-                             postfix[p++]=symbol;
-                        }
-                }
-        }
-        while(!isEmpty( ))
-                postfix[p++]=pop();
-        postfix[p]='\0'; /*End postfix with'\0' to make it a string*/
-}/*End of infix_to_postfix()*/
-
-/*This function returns the priority of the operator*/
-int priority(char symbol)
-{
-        switch(symbol)
-        {
-        case '(':
-                return 0;
-        case '+':
-        case '-':
-                return 1;
-        case '*':
-        case '/':
-        case '%':
-                return 2;
-        case '^':
-                return 3;
-        default :
-                return 0;
-        }
-}/*End of priority()*/
-
-void push(long int symbol)
-{
-        if(top>MAX)
-        {
-                printf("Stack overflow\n");
-                exit(1);
-        }
-        stack[++top]=symbol;
-}/*End of push()*/
-
-long int pop()
-{
-        if( isEmpty() )
-        {
-                printf("Stack underflow\n");
-                exit(1);
-        }
-        return (stack[top--]);
-}/*End of pop()*/
-int isEmpty()
-{
-        if(top==-1)
-                return 1;
-        else
-                return 0;
-}/*End of isEmpty()*/
-
-int white_space(char symbol)
-{
-        if( symbol == BLANK || symbol == TAB )
-                return 1;
-        else
-                return 0;
-}/*End of white_space()*/
-
-long int eval_post()
-{
-        long int a,b,temp,result;
-        unsigned int i;
-
-        for(i=0;i<strlen(postfix);i++)
-        {
-                if(postfix[i]<='9' && postfix[i]>='0')
-                        push(postfix[i]-'0');
-                else
-                {
-                        a=pop();
-                        b=pop();
-                        switch(postfix[i])
-                        {
-                        case '+':
-                                temp=b+a; break;
-                        case '-':
-                                temp=b-a;break;
-                        case '*':
-                                temp=b*a;break;
-                        case '/':
-                                temp=b/a;break;
-                        case '%':
-                                temp=b%a;break;
-                        case '^':
-                                temp=pow(b,a);
-                        }
-                        push(temp);
-                }
-        }
-        result=pop();
-        return result;
-}/*End of eval_post */
+return 0;
+}
